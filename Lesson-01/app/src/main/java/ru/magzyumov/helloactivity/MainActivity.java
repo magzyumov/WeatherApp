e818772c1a2df6dce1d4162c1cf7aa3ceabbf7b4
Ruslan Magzyumov <magzyumov.rr@gmail.com>
2020-03-30 15:09:12 +0500

    First Commit

---
+++ Lesson-01/app/src/main/java/ru/magzyumov/helloactivity/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.magzyumov.helloactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
