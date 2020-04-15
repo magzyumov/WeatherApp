82152b096b00ae247d420e74afb2c722553181f8
Ruslan Magzyumov <magzyumov.rr@gmail.com>
2020-04-15 21:20:49 +0500

    Добавил сохранение данных. Задание выполенено

--- WeatherApp/app/src/main/java/ru/magzyumov/weatherapp/LocationActivity.java
+++ WeatherApp/app/src/main/java/ru/magzyumov/weatherapp/LocationActivity.java
@@ -3,14 +3,18 @@ package ru.magzyumov.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LocationActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
     //Объявляем переменные
     private Toolbar toolbar;
     private SearchView searchView;
     private Intent intent;
+    private String newCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,22 @@ public class LocationActivity extends AppCompatActivity implements SearchView.On
         //Показываем кнопку назад
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState){
+        super.onSaveInstanceState(saveInstanceState);
+        saveInstanceState.putString("newCity", newCity); // Сохраняем введеное значение города
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState){
+        super.onRestoreInstanceState(saveInstanceState);
+        newCity = saveInstanceState.getString("newCity"); // Восстанавливаем введенное значение поиска
+        searchView.setQuery(newCity,false);     //вводим раннее введенный текст
+        TextView tV = findViewById(R.id.textVievLastQuery);
+        tV.setText(newCity);
     }
 
     @Override
@@ -44,6 +64,7 @@ public class LocationActivity extends AppCompatActivity implements SearchView.On
 
     @Override
     public boolean onQueryTextChange(String newText) {
+        newCity = newText;
         return false;
     }
 }
--- WeatherApp/app/src/main/java/ru/magzyumov/weatherapp/MainPresenter.java
+++ WeatherApp/app/src/main/java/ru/magzyumov/weatherapp/MainPresenter.java
@@ -1,5 +1,7 @@
 package ru.magzyumov.weatherapp;
 
+import java.util.Random;
+
 public final class MainPresenter {
     //Внутреннее поле, будет хранить единственный экземпляр
     private static MainPresenter instance = null;
@@ -7,22 +9,46 @@ public final class MainPresenter {
     // Поле для синхронизации
     private static final Object syncObj = new Object();
 
-    // Это наш счетчик
-    private int counter;
+    //Поля для хранения
+    private int currentTemp;           // Поле для хранения температуры
+    private boolean settingNightMode;  //Поле для хранения темного режима
+    private boolean settingTempEu;     //Поле для хранения единиц измерения температуры
+    private boolean settingWindEU;     //Поле для хранения единиц измерения силы ветра
+    private boolean settingPressEU;    //Поле для хранения единиц измерения давления
+    private boolean settingNotice;     //Поле для настроек уведомления
 
     // Конструктор (вызывать извне его нельзя, поэтому он приватный)
     private MainPresenter(){
-        counter = 0;
+        currentTemp = 56;
+        settingNightMode = false;
+        settingTempEu = true;
+        settingWindEU = false;
+        settingPressEU = false;
+        settingNotice = true;
     }
 
-    // Увеличение счетчика
-    public void incrementCounter(){
-        counter++;
+    // Генерируем температуру
+    public void generateCurrentTemp(){
+        Random random = new Random();
+        currentTemp = random.nextInt(50);
     }
 
-    public int getCounter(){
-        return counter;
-    }
+    public int getCurrentTemp(){ return currentTemp; }
+
+    public boolean getSettingNightMode(){ return settingNightMode; }
+    public void setSettingNightMode(boolean settingNightMode){ this.settingNightMode = settingNightMode; }
+
+    public boolean getSettingTempEu(){ return settingTempEu; }
+    public void setSettingTempEu(boolean settingTempEu){ this.settingTempEu = settingTempEu; }
+
+    public boolean getSettingWindEU(){ return settingWindEU; }
+    public void setSettingWindEU(boolean settingWindEU){ this.settingWindEU = settingWindEU; }
+
+    public boolean getSettingPressEU(){ return settingPressEU; }
+    public void setSettingPressEU(boolean settingPressEU){ this.settingPressEU = settingPressEU; }
+
+    public boolean getSettingNotice(){ return settingNotice; }
+    public void setSettingNotice(boolean settingNotice){ this.settingNotice = settingNotice; }
 
     // Метод, который возвращает экземпляр объекта.
     // Если объекта нет, то создаем его.
