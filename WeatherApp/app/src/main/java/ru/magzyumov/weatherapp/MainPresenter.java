911b72fb479031e91e6ccdaa614a07418cb3e128
Ruslan Magzyumov <magzyumov.rr@gmail.com>
2020-04-16 15:27:22 +0500

    Минимизировал код для переключателей

--- WeatherApp/app/src/main/java/ru/magzyumov/weatherapp/MainPresenter.java
+++ WeatherApp/app/src/main/java/ru/magzyumov/weatherapp/MainPresenter.java
@@ -3,6 +3,17 @@ package ru.magzyumov.weatherapp;
 import java.util.Random;
 
 public final class MainPresenter {
+
+    //Набор типов полей
+    public enum Field {
+        CURRENT_TEMP,
+        SETTING_NIGHT_MODE,
+        SETTING_TEMP_EU,
+        SETTING_WIND_EU,
+        SETTING_PRESS_EU,
+        SETTING_NOTICE
+    }
+
     //Внутреннее поле, будет хранить единственный экземпляр
     private static MainPresenter instance = null;
 
@@ -35,20 +46,59 @@ public final class MainPresenter {
 
     public int getCurrentTemp(){ return currentTemp; }
 
-    public boolean getSettingNightMode(){ return settingNightMode; }
-    public void setSettingNightMode(boolean settingNightMode){ this.settingNightMode = settingNightMode; }
+    public void setSwitch(boolean switchPosition, Field field){
+        switch (field) {
+            case SETTING_NIGHT_MODE:
+                setSettingNightMode(switchPosition);
+                break;
+            case SETTING_TEMP_EU:
+                setSettingTempEu(switchPosition);
+                break;
+            case SETTING_WIND_EU:
+                setSettingWindEU(switchPosition);
+                break;
+            case SETTING_PRESS_EU:
+                setSettingPressEU(switchPosition);
+                break;
+            case SETTING_NOTICE:
+                setSettingNotice(switchPosition);
+                break;
+            default:
+                break;
+        }
+    }
 
-    public boolean getSettingTempEu(){ return settingTempEu; }
-    public void setSettingTempEu(boolean settingTempEu){ this.settingTempEu = settingTempEu; }
+    public boolean getSwitch(Field field){
+        switch (field) {
+            case SETTING_NIGHT_MODE:
+                return getSettingNightMode();
+            case SETTING_TEMP_EU:
+                return getSettingTempEu();
+            case SETTING_WIND_EU:
+                return getSettingWindEU();
+            case SETTING_PRESS_EU:
+                return getSettingPressEU();
+            case SETTING_NOTICE:
+                return getSettingNotice();
+            default:
+                return false;
+        }
+    }
 
-    public boolean getSettingWindEU(){ return settingWindEU; }
-    public void setSettingWindEU(boolean settingWindEU){ this.settingWindEU = settingWindEU; }
+    private boolean getSettingNightMode(){ return settingNightMode; }
+    private void setSettingNightMode(boolean settingNightMode){ this.settingNightMode = settingNightMode; }
 
-    public boolean getSettingPressEU(){ return settingPressEU; }
-    public void setSettingPressEU(boolean settingPressEU){ this.settingPressEU = settingPressEU; }
+    private boolean getSettingTempEu(){ return settingTempEu; }
+    private void setSettingTempEu(boolean settingTempEu){ this.settingTempEu = settingTempEu; }
 
-    public boolean getSettingNotice(){ return settingNotice; }
-    public void setSettingNotice(boolean settingNotice){ this.settingNotice = settingNotice; }
+    private boolean getSettingWindEU(){ return settingWindEU; }
+    private void setSettingWindEU(boolean settingWindEU){ this.settingWindEU = settingWindEU; }
+
+    private boolean getSettingPressEU(){ return settingPressEU; }
+    private void setSettingPressEU(boolean settingPressEU){ this.settingPressEU = settingPressEU; }
+
+    private boolean getSettingNotice(){ return settingNotice; }
+    private void setSettingNotice(boolean settingNotice){ this.settingNotice = settingNotice; }
 
     // Метод, который возвращает экземпляр объекта.
     // Если объекта нет, то создаем его.
--- WeatherApp/app/src/main/java/ru/magzyumov/weatherapp/SettingsActivity.java
+++ WeatherApp/app/src/main/java/ru/magzyumov/weatherapp/SettingsActivity.java
@@ -4,16 +4,11 @@ import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+
 
 public class SettingsActivity extends AppCompatActivity {
     private Toolbar toolbar;
-    private Switch switchNightMode;
-    private Switch switchTempEU;
-    private Switch switchWindEU;
-    private Switch switchPressureEU;
-    private Switch switchNotice;
     final MainPresenter presenter = MainPresenter.getInstance();
 
     @Override
@@ -21,13 +16,16 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-
         //Устанавливаем Toolbar
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //Инициализируем переключатели настроек
-        initSwitch();
+        //Инициализируем переключатели
+        initSwitch((Switch) findViewById(R.id.switchNightMode), MainPresenter.Field.SETTING_NIGHT_MODE);
+        initSwitch((Switch) findViewById(R.id.switchTempEU), MainPresenter.Field.SETTING_TEMP_EU);
+        initSwitch((Switch) findViewById(R.id.switchWindEU), MainPresenter.Field.SETTING_WIND_EU);
+        initSwitch((Switch) findViewById(R.id.switchPressureEU), MainPresenter.Field.SETTING_PRESS_EU);
+        initSwitch((Switch) findViewById(R.id.switchNotice), MainPresenter.Field.SETTING_NOTICE);
 
         //Меняем текст в шапке
         getSupportActionBar().setTitle(R.string.menu_settings);
@@ -37,70 +35,16 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
-    private void initSwitch(){
-        switchNightMode = findViewById(R.id.switchNightMode);
-        switchNightMode.setChecked(presenter.getSettingNightMode());
-        switchNightMode.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+    private void initSwitch(Switch view, final MainPresenter.Field field)  {
+        Switch switchButton = view;
+        switchButton.setChecked(presenter.getSwitch(field));
+        switchButton.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    presenter.setSettingNightMode(true);
-                } else {
-                    presenter.setSettingNightMode(false);
-                }
-            }
-        });
-
-        switchTempEU = findViewById(R.id.switchTempEU);
-        switchTempEU.setChecked(presenter.getSettingTempEu());
-        switchTempEU.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    presenter.setSettingTempEu(true);
-                } else {
-                    presenter.setSettingTempEu(false);
-                }
-            }
-        });
-
-        switchWindEU = findViewById(R.id.switchWindEU);
-        switchWindEU.setChecked(presenter.getSettingWindEU());
-        switchWindEU.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    presenter.setSettingWindEU(true);
-                } else {
-                    presenter.setSettingWindEU(false);
-                }
-            }
-        });
-
-        switchPressureEU = findViewById(R.id.switchPressureEU);
-        switchPressureEU.setChecked(presenter.getSettingPressEU());
-        switchPressureEU.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    presenter.setSettingPressEU(true);
-                } else {
-                    presenter.setSettingPressEU(false);
-                }
-            }
-        });
-
-        switchNotice = findViewById(R.id.switchNotice);
-        switchNotice.setChecked(presenter.getSettingNotice());
-        switchNotice.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    presenter.setSettingNotice(true);
-                } else {
-                    presenter.setSettingNotice(false);
-                }
+                presenter.setSwitch(isChecked, field);
             }
         });
     }
+
+
 }
